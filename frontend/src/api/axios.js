fd8c3c5e5c411b032f56d8c23157c99f8a048e26
Shahnaz Aqsa <shahnazaqsabscs19@gmail.com>
2025-08-25 08:49:03 +0500

    File Updated

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://shahnaz999aqsa.pythonanywhere.com/api",
+    baseURL: "https://shahnaz999aqsa.pythonanywhere.com/api/",
 });
 
 export default api;
