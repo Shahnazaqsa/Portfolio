ab51880e3da5d94ecb11642f067be9f0e6d72694
Shahnaz Aqsa <shahnazaqsabscs19@gmail.com>
2025-10-22 22:06:30 +0500

    update API URL

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Chatbot from "./components/Chatbot";
 import Section from "./components/Section";
+
 
 function App() {
   return (
@@ -33,6 +35,8 @@ function App() {
         <Section direction="up">
           <Contact />
         </Section>
+
+        <Chatbot />
       </div>
     </div>
   );
