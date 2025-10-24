0f82a605f55c6256be6c4bf847e5ad8cb3903f85
Shahnaz Aqsa <shahnazaqsabscs19@gmail.com>
2025-10-25 00:45:35 +0500

    Updated Portfolio

--- backend/aqsa_codes/aqsa_portfolio/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AqsaPortfolioConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'aqsa_portfolio'
--- backend/aqsa_codes/aqsa_portfolio/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import ProjectView, ProjectDetailVeiw, ContactView
-
-urlpatterns = [
-    path("projects/", ProjectView.as_view(), name="project_view"),
-    path("projects/<int:pk>/", ProjectDetailVeiw.as_view(), name="project_detail_view"),
-    path("contact/", ContactView.as_view(), name="contact_view"),
-]
