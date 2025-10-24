95e20350ed5675366f8bef35ae7037c34665e439
Shahnaz Aqsa <shahnazaqsabscs19@gmail.com>
2025-10-24 20:42:38 +0500

    updated chatbox.jsx

--- frontend/src/components/Chatbot.jsx
+++ frontend/src/components/Chatbot.jsx
@@ -15,7 +15,7 @@ const Chatbot = () => {
     setLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/chat/", {
+      const response = await fetch("https://shahnaz999aqsa.pythonanywhere.com/chat/", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ message: input }),
